f49091dc020869349a4699ed48f901e5de3ed1e2
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-02 23:43:12 +0900

    ✨ 関数を定義

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -1,4 +1,8 @@
 const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 
-text.textContent = `カラーコード：${color.value}`;
+// カラーピッカーが変更されたら、カラーコードを表示
+// 無名関数を使っている
+color.addEventListener('input', () => {
+  text.textContent = `カラーコード：${color.value}`;
+});
