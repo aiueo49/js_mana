a9464fce114ec4fd97d75298d611bf24b6bb4529
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-03 00:01:48 +0900

    ✨ 選択した色を背景色に設定

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -4,5 +4,9 @@ const color = document.querySelector('#colorPicker');
 // カラーピッカーが変更されたら、カラーコードを表示
 // 無名関数を使っている
 color.addEventListener('input', () => {
+  // 選択した色を背景色に設定
+  document.body.style.backgroundColor = color.value;
+  // カラーコードを表示
   text.textContent = `カラーコード：${color.value}`;
+
 });
