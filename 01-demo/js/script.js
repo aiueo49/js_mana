ed4bd8423fd637d74bc8aee86334b83f5f18526f
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-06-03 16:59:15 +0900

    ✨ スクリーンが下から上へ

--- 01-demo/js/script.js
+++ 01-demo/js/script.js
@@ -2,6 +2,7 @@
 ローディングから画面遷移
 ================================================== */
 const loadingAreaGrey = document.querySelector('#loading');
+const loadingAreaGreen = document.querySelector('#loading-screen');
 
 window.addEventListener('load', () => {
   // ローディング中（グレースクリーン） 
@@ -13,6 +14,19 @@ window.addEventListener('load', () => {
     {
       duration: 2000,
       delay: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+
+  // ローディング中（グリーンスクリーン）
+  loadingAreaGreen.animate(
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh'],
+    },
+    {
+      duration: 2000,
+      delay: 800,
       easing: 'ease',
       fill: 'forwards',
     }
